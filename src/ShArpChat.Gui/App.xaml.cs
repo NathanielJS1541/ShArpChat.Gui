28c3d85887c3ab1af3c0983eb138280a79666ce1
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-04-14 13:08:50 +0100

    Added template MAUI app to src/

---
+++ src/ShArpChat.Gui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShArpChat.Gui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/ShArpChat.Gui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShArpChat.Gui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
